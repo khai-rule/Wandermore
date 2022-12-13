7e493c20855d7903a318760bd5f37d11e0481a2b
Khairul <Qai-rule@hotmail.com>
2022-12-13 10:23:07 +0800

    set up router

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
 // Dependencies
 require("dotenv").config();
+const path = require("path")
 const express = require("express");
+const mongoose = require("mongoose")
 const morgan = require("morgan")
 
 // Configuration
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI);
 
 // Middleware
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.static("../client/dist"));
 
 app.get("/", (req, res) => {
     res.json({ msg: "Hello World!" });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("..", "client", "dist", "index.html"));
+});
   
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+mongoose.connection.once("open", () => {
+    console.log("connected to mongoose...");
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
 });
