03a69290d6728d549cefa9d393de019811ca624f
Khairul <Qai-rule@hotmail.com>
2022-12-13 20:38:52 +0800

    fetched data from database

--- client/pages/AboutYou.jsx
+++ client/pages/AboutYou.jsx
@@ -1,19 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const AboutYou = () => {
-    const [search, setSearch] = useState("")
+    const [search, setSearch] = useState("");
+    const [inDatabase, setInDatabase] = useState();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(search)
     }
 
+    const fetchData = async () => {
+    try {
+    const request = await fetch("/api/aboutyou")
+    if (!request.ok) {
+    throw new Error ("Network error");
+    }
+    const data = await request.json();
+    setInDatabase(data);
+    console.log("data", data)
+    } catch (error) {
+    console.error(error);
+        };
+    };
+    
+    useEffect(() => {fetchData()}, []);
+
+    const textBox = () => {
+        inDatabase.map((item) => {
+            return (
+                <div>
+                <label for="username">Name:</label>
+                <input type="text" name="username" />
+                </div>
+            )
+        })
+    }
+    
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <h1>About You</h1>
                 <label>
                     <input type="text" name="search" placeholder="Date of Birth" onChange={e => setSearch(e.target.value)}/>
+                    <input type="text" name="search" placeholder="Hobbies" onChange={e => setSearch(e.target.value)}/>
+                    <input type="text" name="search" placeholder="Hobbies" onChange={e => setSearch(e.target.value)}/>
+                    <input type="text" name="search" placeholder="Hobbies" onChange={e => setSearch(e.target.value)}/>
                     <input type="text" name="search" placeholder="Hobbies" onChange={e => setSearch(e.target.value)}/>
                 </label>
                 <input type="submit" value ="Submit"/>
--- server/controllers/aboutYou/seed.js
+++ server/controllers/aboutYou/seed.js
@@ -1,23 +1,13 @@
 const AboutYou = require("../../models/AboutYou");
 
 const seed = async (req, res) => {
-  const seedAboutYou = [
-    {
+  const seedAboutYou = {
       dateOfBirth: 20081996,
       hobbies: ["Cycling", "Bowling"],
       countryOfResidence : "Singapore",
       dietaryRestrictions: "Vegan",
       accessibility: "Nil"
-  },
-  {
-    dateOfBirth: 20081926,
-    hobbies: ["Cycling", "Water Sports"],
-    countryOfResidence : "Malaysia",
-    dietaryRestrictions: "Herbivore",
-    accessibility: "Nil"
-},
-
-  ];
+  };
   await AboutYou.deleteMany({});
 
   const aboutYou = await AboutYou.insertMany(seedAboutYou);
