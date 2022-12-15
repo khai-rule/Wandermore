169bfbbe082224f2b02ca9a10670bd0747d182b6
Nicholas Tan <nicholasgtan@gmail.com>
2022-12-15 17:01:31 +0800

    Updated Login page with Formik/Yup!

--- client/src/components/validation/schema.js
+++ client/src/components/validation/schema.js
@@ -17,3 +17,8 @@ export const signUpSchema = yup.object().shape({
     .oneOf([yup.ref("password"), null], "Passwords must match")
     .required("Required"),
 });
+
+export const loginSchema = yup.object().shape({
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  password: yup.string().required("Required"),
+});
