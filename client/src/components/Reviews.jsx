d84bd3c971c3b12aa27ff488a08ef59ceba34d66
Khairul <Qai-rule@hotmail.com>
2022-12-15 16:33:36 +0800

    added header, reviews and categories section for homepage

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '../components/mui-components/Typography';
+
+const item = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  px: 5,
+};
+
+function ProductValues() {
+  return (
+    <Box
+      component="section"
+      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'secondary.light' }}
+    >
+      <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
+        <Box
+          component="img"
+          src="/static/themes/onepirate/productCurvyLines.png"
+          alt="curvy lines"
+          sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
+        />
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src="/static/themes/onepirate/productValues1.svg"
+                alt="suitcase"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 2 }}>
+                Sam, Software Engineer
+              </Typography>
+              <Typography variant="p">
+                {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                }
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src="/static/themes/onepirate/productValues2.svg"
+                alt="graph"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 2 }}>
+                Yuri, Artist
+              </Typography>
+              <Typography variant="p">
+                {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                }
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <Box
+                component="img"
+                src="/static/themes/onepirate/productValues3.svg"
+                alt="clock"
+                sx={{ height: 55 }}
+              />
+              <Typography variant="h6" sx={{ my: 2 }}>
+                Logan, Technician
+              </Typography>
+              <Typography variant="p">
+                {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                }
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default ProductValues;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header.jsx"
+import Reviews from "../components/Reviews.jsx"
+import TypesOfTrip from "../components/TypesOfTrip.jsx"
 
 const Home = () => {
   return (
     <div>
-      Home Page
+    <Header />
+    <Reviews />
+    <TypesOfTrip />
       <br />
       New to Wandermore? Sign up <Link to="/signup">here</Link>. <br />
       Already a user? Login <Link to="/login">here</Link>.
