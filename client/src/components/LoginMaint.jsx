10ccd1eff92749d0dd9bef89a62989dfc9ff31ca
Nicholas Tan <nicholasgtan@gmail.com>
2022-12-18 00:05:03 +0800

    Created LoginMaint page to allow users to change their password

---
+++ client/src/components/LoginMaint.jsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+import { Form, Formik } from "formik";
+import CustomInput from "../components/CustomInput";
+import { loginMaintSchema } from "../components/validation/schema";
+
+const LoginMaint = ({ loginID }) => {
+  const [inDatabase, setInDatabase] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    passwordOld: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [msg, setMsg] = useState("");
+
+  //? Formik submit handler (line 50 - 69)
+  const handlePasswordChange = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    try {
+      const res = await fetch(`/api/user/${loginID}`);
+      const data = await res.json();
+      if (values.passwordOld === data.password) {
+        if (values.passwordOld === values.password) {
+          actions.resetForm();
+          setMsg("Your new password is the same. Nothing happened.");
+        } else {
+          const response = await fetch(`/api/user/${loginID}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+          });
+          if (response.ok) {
+            actions.resetForm();
+            setMsg("Your password has been changed!");
+          }
+        }
+      } else {
+        actions.resetForm();
+        setMsg("Your old password is incorrect, please try again");
+      }
+    } catch (error) {
+      throw new Error("Network response was not OK");
+    }
+  };
+
+  //! Fetch Data
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`/api/user/database/${loginID}`);
+      try {
+        if (!response.ok) {
+          throw new Error("Network error");
+        }
+        const data = await response.json();
+        if (data !== null) {
+          setInDatabase({ ...inDatabase, ...data });
+        }
+      } catch (error) {
+        throw new Error("Network response was not OK");
+      }
+    };
+    fetchData();
+  }, [loginID]);
+
+  return (
+    <div>
+      <Formik
+        enableReinitialize={true}
+        initialValues={inDatabase}
+        validationSchema={loginMaintSchema}
+        onSubmit={handlePasswordChange}
+      >
+        {({ isSubmitting }) => (
+          <Form autoComplete="off">
+            <fieldset>
+              <legend>Login Info</legend>
+              <CustomInput
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="Enter your email"
+                disabled={true}
+              />
+              <br />
+              <CustomInput
+                label="First Name"
+                name="firstName"
+                type="text"
+                placeholder="Enter your first name"
+                disabled={true}
+              />
+              <br />
+              <CustomInput
+                label="Last Name"
+                name="lastName"
+                type="text"
+                placeholder="Enter your last name"
+                disabled={true}
+              />
+              <br />
+              <CustomInput
+                label="Old Password"
+                name="passwordOld"
+                type="password"
+                placeholder="Enter your Old Password"
+              />
+              <br />
+              <CustomInput
+                label="New Password"
+                name="password"
+                type="password"
+                placeholder="Enter your New Password"
+              />
+              <br />
+              <CustomInput
+                label="Confirm New Password"
+                name="confirmPassword"
+                type="password"
+                placeholder="Enter your New Password again"
+              />
+              <br />
+              <button type="submit" disabled={isSubmitting}>
+                Change Password
+              </button>{" "}
+              <span>{msg}</span>
+            </fieldset>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginMaint;
--- client/src/pages/AboutYou.jsx
+++ client/src/pages/AboutYou.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AboutYouForm from "../components/AboutYouForm";
+import LoginMaint from "../components/LoginMaint";
 
 const AboutYou = ({ notLoggedIn, setNotLoggedIn, loginID, user }) => {
+  const [view, setView] = useState("aboutYou");
   const navigate = useNavigate();
   //* Code to push back to login page if not Logged in
   useEffect(() => {
@@ -17,10 +20,39 @@ const AboutYou = ({ notLoggedIn, setNotLoggedIn, loginID, user }) => {
 
   return (
     <div>
+      <nav>
+        <div>
+          <span
+            onClick={() => setView("aboutYou")}
+            style={{
+              color: view === "aboutYou" ? "#000000" : "#808080",
+              textDecoration: view === "aboutYou" ? "underline" : "0",
+              cursor: "pointer",
+            }}
+          >
+            About You
+          </span>
+          {" | "}
+          <span
+            onClick={() => setView("loginInfo")}
+            style={{
+              color: view === "loginInfo" ? "#000000" : "#808080",
+              textDecoration: view === "loginInfo" ? "underline" : "0",
+              cursor: "pointer",
+            }}
+          >
+            Login Info
+          </span>
+        </div>
+      </nav>
+      {view === "aboutYou" ? (
+        <AboutYouForm loginID={loginID} user={user} />
+      ) : (
+        <LoginMaint loginID={loginID} />
+      )}
       <Link to="/triprequest">
         <button>New Trip +</button>
       </Link>
-      <AboutYouForm loginID={loginID} user={user}/>
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
