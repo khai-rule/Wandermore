721237a72ad039314b41da5282b38dc423f93269
Nicholas Tan <nicholasgtan@gmail.com>
2022-12-18 13:03:33 +0800

    Moved userSeed to a separate seed file and linked back to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,11 @@
 // const bcrypt = require("bcrypt");
 const express = require("express");
 const User = require("../models/User");
+const userSeed = require("./seeds/userSeed");
 const userRouter = express.Router();
 const ObjectId = require("mongodb").ObjectId;
+
+userRouter.get("/seed", userSeed);
 
 userRouter.get("/", async (req, res) => {
   try {
@@ -67,42 +70,6 @@ userRouter.put("/:id", async (req, res) => {
     res.json(userUpdate);
   } catch (error) {
     res.status(500).json({ error });
-  }
-});
-
-userRouter.get("/seed", async (req, res) => {
-  //   const saltRounds = 10;
-  const users = [
-    {
-      email: "johntan@gmail.com",
-      firstName: "John",
-      lastName: "Tan",
-      password: "jt1234",
-
-      //   password: bcrypt.hashSync("1q2w3e4r", saltRounds),
-    },
-    {
-      email: "bobbylee@hotmail.com",
-      firstName: "Bobby",
-      lastName: "Lee",
-      password: "bl4321",
-
-      //   password: bcrypt.hashSync("qawsedrf", saltRounds),
-    },
-    {
-      email: "janegoh@msn.com",
-      firstName: "Jane",
-      lastName: "Goh",
-      password: "Jg1111",
-      //   password: bcrypt.hashSync("azsxdcfv", saltRounds),
-    },
-  ];
-  try {
-    await User.deleteMany({});
-    const newUser = await User.create(users);
-    res.json(newUser);
-  } catch (error) {
-    res.status(500).json(error);
   }
 });
 
